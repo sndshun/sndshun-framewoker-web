5078936d879b8a564b84a1d8f61e30b474fa067a
hlmy <yodc887@gmail>
2023-12-06 10:57:12 +0800

    新增头像显示、增加动画

--- src/blog/page/About.vue
+++ src/blog/page/About.vue
@@ -1,11 +1,37 @@
 <script setup>
-
 </script>
-
 <template>
-  <h1>关于</h1>
+  <section class="bg-white dark:bg-gray-900">
+    <div id="about-home">
+      <!-- 关于我起始 -->
+      <div class="author-info">
+        <div class="flex space-x-4 animate-pulse">
+          <span class="relative inline-block">
+            <img
+              src="https://ooo.0x0.ooo/2023/05/05/Cyfvi.jpg"
+              class="object-cover w-16 h-16 rounded-full"
+            />
+          </span>
+        </div>
+      </div>
+    </div>
+  </section>
 </template>
 
 <style scoped>
+#about-home {
+  display: flex;
+  padding-top: 1rem;
+  flex-direction: column;
+  align-items: center;
+}
+#about-home .author-info {
+  display: flex;
+  align-items: center;
+  margin: 0 0 16px 0;
+}
 
+.animate-pulse {
+  animation: pulse 1.5s cubic-bezier(0.4, 0, 0.6, 1) infinite;
+}
 </style>
